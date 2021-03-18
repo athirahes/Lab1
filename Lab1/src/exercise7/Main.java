ea9683811d46fe28014dd4e72632b15a44925dea
Shafiqah Nur Athirah Binti Hassan <shafiqahnurathirah@gmail.com>
2021-03-19 00:45:48 +0800

    Test Push Commit

--- Lab1/src/exercise7/Main.java
+++ Lab1/src/exercise7/Main.java
@@ -1,11 +1,12 @@
 package exercise7;
+
 import exercise8.WeightList;
 
 public class Main {
 	public static void main(String args[]) {
-		WeightOnMars wMars = new WeightOnMars(110);
+		WeightOnMars wMars = new WeightOnMars(65);
 		wMars.calculateWeight();
-		
-		WeightList wList = new WeightList(); 
-	 }
+
+		WeightList wList = new WeightList();
+	}
 }
--- Lab1/src/exercise7/WeightOnMars.java
+++ Lab1/src/exercise7/WeightOnMars.java
@@ -2,20 +2,21 @@ package exercise7;
 
 public class WeightOnMars {
 	double mass;
-	
-	public WeightOnMars(double mass){
+
+	public WeightOnMars(double mass) {
 		this.mass = mass;
 	}
-	
+
 	void setMass(double mass) {
 		this.mass = mass;
 	}
-	
+
 	public void calculateWeight() {
-		double weightOnEarth = mass * 9.80665;
+		double weightOnEarth = mass * 9.807;
 		double weightOnMars = mass * 3.711;
 
-		System.out.println("Mass: "+ mass +"kg\nWeight on Earth: " + weightOnEarth +"N\nWeight on Mars: "+ weightOnMars +"N");
-		System.out.println("Mass: "+ mass +"kg\nWeight on Earth: " + weightOnEarth +"N\nWeight on Mars: "+ weightOnMars +"N\n");
+		System.out.println("Mass: " + mass + "kg");
+		System.out.println("Weight on Earth: " + weightOnEarth + "N");
+		System.out.println("Weight on Mars: " + weightOnMars + "N\n");
 	}
 }
