4af06cc515bf755b9ed1b1620d2abdb921e11c42
Shafiqah Nur Athirah Binti Hassan <shafiqahnurathirah@gmail.com>
2021-03-18 23:20:07 +0800

    Initial Commit - Exercise 7

---
+++ Lab1/src/exercise7/Main.java
@@ -0,0 +1,8 @@
+package exercise7;
+
+public class Main {
+	public static void main(String args[]) {
+		WeightOnMars wMars = new WeightOnMars(110);
+		wMars.calculateWeight();
+	 }
+}
---
+++ Lab1/src/exercise7/WeightOnMars.java
@@ -0,0 +1,23 @@
+package exercise7;
+
+public class WeightOnMars {
+	
+	double mass;
+	
+	public WeightOnMars(double mass){
+		this.mass = mass;
+	}
+	
+	void setMass(double mass) {
+		this.mass = mass;
+	}
+	
+	public void calculateWeight() {
+		double weightOnEarth = mass * 9.80665;
+		double weightOnMars = mass * 3.711;
+
+		System.out.println("Mass: "+ mass +"kg\nWeight on Earth: " + weightOnEarth +"N\nWeight on Mars: "+ weightOnMars +"N");
+		System.out.println("Mass: "+ mass +"kg\nWeight on Earth: " + weightOnEarth +"N\nWeight on Mars: "+ weightOnMars +"N\n");
+	}
+	
+}
